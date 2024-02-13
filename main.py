444d2c4c5b175ec7dc3ee3b31a6362bc47d4909c
Ammar-Azman <muhammarsafuan@gmail.com>
2024-02-13 15:42:54 +0800

    feat: added basic ui frame

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import streamlit as st
 from components import side_bar, page_title
+from model import create_agent, ask_agent
+from data import Dataset
 
-st.set_page_config(page_title="Decoris Chatbot", page_icon="🤖")
+def main():
+    st.set_page_config(page_title="Decoris Chatbot", page_icon="🤖")
 
 
-page_title("Decoris Chatbot with LLM")
-side_bar()
+    page_title("Decoris Chatbot with LLM")
+    side_bar()
 
-user_input = st.chat_input("Ask about the Campaign, Adset or Ads!")
+    data = Dataset()
+    if not data.parquet_exist():
+        data.load_parquet()
+    
+    agent = create_agent(dataset=data.get_merge_df())
+
+    user_input = st.chat_input("Ask about the Campaign, Adset or Ads!")
+    if user_input:
+        response = ask_agent(agent, user_input)
+        st.write(response)
+
+if __name__ == "__main__":
+    main()
