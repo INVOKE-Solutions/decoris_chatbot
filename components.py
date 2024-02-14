cac9128338e13a59bccd06f4993ea83353538b7c
Ammar-Azman <muhammarsafuan@gmail.com>
2024-02-14 09:23:10 +0800

    feat: added memory for agent

--- components.py
+++ components.py
@@ -1,6 +1,8 @@
 import time
 import streamlit as st
 from data import Dataset
+from css_template import user_template, bot_template
+
 
 def side_bar():
     data = Dataset()
@@ -12,9 +14,34 @@ def side_bar():
             data.load_parquet()
             for i in range(100):
                 time.sleep(0.001)
-                progress_bar.progress(i+1)
+                progress_bar.progress(i + 1)
             time.sleep(0.5)
             st.success("Refresh complete")
 
-def page_title(title:str):
+
+def page_title(title: str):
     st.title(title)
+
+
+def initial_chat_history_state():
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+
+def show_chat_dialogue(chat_history: list):
+    """
+    Initialize state to ensure the apps
+    remember the conversation.
+
+    """
+    for i, message in enumerate(chat_history):
+        if i % 2 == 0:
+            st.write(
+                user_template.replace("{{MSG}}", f"{message}"),
+                unsafe_allow_html=True,
+            )
+        else:
+            st.write(
+                bot_template.replace("{{MSG}}", f"🤖-- {message}"),
+                unsafe_allow_html=True,
+            )
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import streamlit as st
-from components import side_bar, page_title
+from components import (
+    side_bar,
+    page_title,
+    initial_chat_history_state,
+    show_chat_dialogue,
+)
 from model import create_agent, ask_agent
 from data import Dataset
+from css_template import user_template, bot_template, css
+from langchain.memory import ConversationBufferMemory
+
 
 def main():
     st.set_page_config(page_title="Decoris Chatbot", page_icon="🤖")
-
 
     page_title("Decoris Chatbot with LLM")
     side_bar()
@@ -13,13 +20,21 @@ def main():
     data = Dataset()
     if not data.parquet_exist():
         data.load_parquet()
-    
-    agent = create_agent(dataset=data.get_merge_df())
 
+    # initial state
+    initial_chat_history_state()
+
+    agent = create_agent(dataset=data.get_merge_df())
+    st.write(css, unsafe_allow_html=True)
     user_input = st.chat_input("Ask about the Campaign, Adset or Ads!")
+
     if user_input:
-        response = ask_agent(agent, user_input)
-        st.write(response)
+        st.session_state.chat_history.append(user_input)
+        agent_response = ask_agent(agent, st.session_state.chat_history)
+        st.session_state.chat_history.append(agent_response["output"])
+
+        show_chat_dialogue(st.session_state.chat_history)
+
 
 if __name__ == "__main__":
     main()
