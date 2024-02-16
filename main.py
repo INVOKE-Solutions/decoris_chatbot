a77f9f9c0b44942162c3f4f763bf205d04a2112a
Ammar-Azman <muhammarsafuan@gmail.com>
2024-02-16 17:10:12 +0800

    feat: added login feature for main page

--- main.py
+++ main.py
@@ -2,42 +2,56 @@ import streamlit as st
 from components import (
     side_bar,
     page_title,
-    initial_chat_history_state,
     show_chat_dialogue,
     update_chat_history,
     handle_bot_response,
+    initialize_login_state,
+    initialize_chat_history_state,
 )
 from data import Dataset
 from model import PandasAgentWithMemory
+from st_pages import show_pages, Page
+
+# Rearrange page order
+show_pages(
+    [
+        Page("pages/login.py", "Login", "🔐"),
+        Page("main.py", "Chatbot", "🤖"),
+    ]
+)
 
 
 def main():
-    st.set_page_config(page_title="Decoris Chatbot", page_icon="🤖")
+    initialize_login_state()
+    if not st.session_state.login:
+        st.warning("Please login first")
+    elif st.session_state.login:
+        page_title("Decoris Chatbot 🤖")
+        side_bar()
 
-    page_title("Decoris Chatbot - 🤖")
-    side_bar()
+        # download data
+        data = Dataset()
+        if not data.parquet_exist():
+            data.download_parquet()
 
-    # download data
-    data = Dataset()
-    if not data.parquet_exist():
-        data.download_parquet()
+        # initial state
+        initialize_chat_history_state()
 
-    # initial state
-    initial_chat_history_state()
+        # create agent (model)
+        agent = PandasAgentWithMemory(data.get_merge_df())
 
-    # create agent (model)
-    agent = PandasAgentWithMemory(data.get_merge_df())
+        # user input feature
+        user_input = st.chat_input("Ask about the Campaign, Adset or Ads!")
 
-    # user input feature
-    user_input = st.chat_input("Ask about the Campaign, Adset or Ads!")
-
-    if user_input:
-        bot_response = handle_bot_response(
-            agent, user_input, chat_history=st.session_state.chat_history
-        )
-        update_chat_history(user_input, bot_response)
-        show_chat_dialogue(st.session_state.chat_history)
+        if user_input:
+            bot_response = handle_bot_response(
+                agent, user_input, chat_history=st.session_state.chat_history
+            )
+            update_chat_history(user_input, bot_response)
+            show_chat_dialogue(st.session_state.chat_history)
 
 
 if __name__ == "__main__":
+    st.set_page_config(page_title="Decoris Chatbot", page_icon="🤖")
+
     main()
