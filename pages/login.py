7c2bd0d16bf27058e684002a0d9f851c1de6d6d6
Ammar-Azman <muhammarsafuan@gmail.com>
2024-02-16 17:09:40 +0800

    feat: add login page

---
+++ pages/login.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+def login():
+    with st.form("Login", border=False):
+        st.title("Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        submit_button = st.form_submit_button("Submit")
+
+        if submit_button:
+            if (
+                username == st.secrets["USERNAME"]
+                and password == st.secrets["PASSWORD"]
+            ):
+                st.success(f"Welcome {username}")
+                st.session_state.login = True
+                return True
+            else:
+                st.error("Username/password is incorrect")
+        else:
+            st.warning("Please enter username and password")
+
+
+if __name__ == "__main__":
+    login()
